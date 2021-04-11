582661e78b7b00c03e1e47eae365380809e6c864
Karl Stencell <Kstencell6368@conestogac.on.ca>
2021-04-11 18:48:15 -0400

    added indicator files from systematic trading branch to this branch

---
+++ MASTR/indicators.h
@@ -0,0 +1,11 @@
+#pragma once
+
+typedef struct indicators {
+	float SMA10;
+	float SMA25;
+	float SMA50;
+}INDICATORS, *P_INDICATORS;
+
+P_INDICATORS createIndicators();
+void computeIndicators(P_DATA_LIST);
+float computeSMA(struct dataNode*, int);
