dbd650ac3985d1f156dc8eedd083a7ee62d4b81d
Karl Stencell <Kstencell6368@conestogac.on.ca>
2021-04-11 22:00:33 -0400

    Revert "added in EMA functionality" - This is in the wrong branch.
    This reverts commit 14457c13b7f6815cc199ab0b07675fc20e3455ca.

--- MASTR/indicators.c
+++ MASTR/indicators.c
@@ -25,9 +25,7 @@ void computeIndicators(P_DATA_LIST list) {
 		current->nodeData->indicators->SMA10 = 0;
 		current->nodeData->indicators->SMA25 = 0;
 		current->nodeData->indicators->SMA50 = 0;
-		current->nodeData->indicators->EMA5 = 0;
-		if (i > 4)
-			current->nodeData->indicators->EMA5 = computeEMA(current);
+
 		if (i > 8)
 			current->nodeData->indicators->SMA10 = computeSMA(current, 10);
 		if (i > 23)
@@ -52,30 +50,4 @@ float computeSMA(P_DATA_NODE current, int duration) {
 	float SMA = runningTotal / duration;
 
 	return SMA;
-}
-
-float computeEMA(P_DATA_NODE current) {
-	float closingPriceOne = 0.0, emaPrev = 0.0, smoothFactor = 2.0, numDays = 6.0;  // numDays -- portion of ema equation is smooth / 1 + desired days (5)
-	float EMA = 0.0;
-
-		if (current->prev->nodeData->indicators->EMA5 == 0) {
-
-			closingPriceOne = current->nodeData->close;
-			emaPrev = computeSMA(current, 5);
-
-	    }
-		else {
-			closingPriceOne = current->nodeData->close;
-			getPrevNode(current);
-			emaPrev = current->nodeData->indicators->EMA5;
-		}
-
-	EMA = closingPriceOne * (smoothFactor / numDays) +  emaPrev * (1 - (smoothFactor / numDays));
-	return EMA;
-
-}
-
-void getPrevNode(P_DATA_NODE current) {
-	current = current->prev;
-	return current;
 }
--- MASTR/indicators.h
+++ MASTR/indicators.h
@@ -4,12 +4,8 @@ typedef struct indicators {
 	float SMA10;
 	float SMA25;
 	float SMA50;
-	float EMA5;
 }INDICATORS, *P_INDICATORS;
 
 P_INDICATORS createIndicators();
 void computeIndicators(P_DATA_LIST);
 float computeSMA(struct dataNode*, int);
-float computeEMA(struct dataNode*);
-void getPrevNode(struct dataNode*); 
-
