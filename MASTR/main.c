c2f0c3797fa9d9262839720faf956a7a3cd7846b
kds4 <71084029+kds4@users.noreply.github.com>
2021-04-14 18:26:53 -0400

    Revert "I found a library to plot data. Need to catch this branch up with the working prototype to test out the library. (#24)"
    This reverts commit 3575dc651070894015d07baac11c71b0ba4a6b68.

--- MASTR/main.c
+++ MASTR/main.c
@@ -1,31 +1,8 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include "historicalData.h"
-#include "dataNode.h"
-#include "data.h"
-#include "indicators.h"
-#include "tradeConditionList.h"
-#include "tradeCondition.h"
-#include "mainMenu.h"
 
-int main(int argc, char* argv[]) {
+int main(void) {
 
-	P_DATA_LIST historicalData = createList();
-
-	if (!loadDataFromDisk(historicalData, argv[1])) {
-		fprintf(stderr, "No historical data found.");
-		exit(EXIT_FAILURE);
-	}
-
-	computeIndicators(historicalData);
-
-	P_TRADE_CONDITION_LIST tradeConditionList = initializeTradeConditionList();
-
-	bool continueMainMenu = true;
-	do {
-		printMainMenuOptions();
-		continueMainMenu = executeMainMenuOptionChoice(historicalData, tradeConditionList);
-	} while (continueMainMenu);
+	printf("Hello group!");
 
 	return 0;
 }
